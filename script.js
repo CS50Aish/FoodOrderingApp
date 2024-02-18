872f3c1aa425e8ffef0f0dbb26c08aa7eacfd072
aishwarya <aishwaryawaghmare15@gmail.com>
2024-02-18 18:38:35 +0000

    Food Delivery Online Application HTML CSS JavaScript Changes Updated

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Navigation Bar
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
   navbar.classList.toggle('active');
 };
+
+// Search Bar Functionality
+
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle('active');
+}
